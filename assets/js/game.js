53acc3747421fb592acb1a9b84b02321a9835a6c
blake allen <csgo6811@gmail.com>
2021-12-11 10:07:39 -0500

    added base stats for charactor

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quatation marks around playerName
+window.alert(playerName);
+
+
 // this creates a function named "fight"
 
 function fight () {
     window.alert("the fight has begun!");
 }
+// fight();
